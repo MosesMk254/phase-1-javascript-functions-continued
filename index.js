f3c3b304f6d6a6e42f9914664d3aafde79787a8c
Moses Mutisya <mutisyamoses722@gmail.com>
2024-03-20 15:48:58 +0300

    working with functions

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // code your solution here
+// function saturdayFun(
+//   activity1,
+//   activity = "This Saturday, I want to roller-skate!"
+// ) {
+//   if (activity1 === true) {
+//     return ` This Saturday, I want to  ${activity1}`;
+//   } else {
+//     return activity;
+//   }
+// }
+
+// function saturdayFun(msg = "This Saturday, I want to") {
+//   const activity1 = function (msg, activity = "roller-skate") {
+//     return `${msg} ${activity}!`;
+//   };
+//   return activity1('This Saturday, I want to', 'roller-skate')
+// }
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+
+const mondayWork = function (activity = "go to the office") {
+  return `This Monday, I will ${activity}.`;
+};
+
+function wrapAdjective(flair = '*') {
+  return function(adjective) {
+      return `You are ${flair}${adjective}${flair}!`;
+  }
+}
